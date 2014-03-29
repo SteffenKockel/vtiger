b095eccf8287bad10eaa9bb0ee6c0f8c7a8a3529
Steffen Kockel <an@steffen-kockel.de>
2014-03-29 16:13:59 +0100

    DB helper script modiefied

--- tools/re_install_sql.sh
+++ tools/re_install_sql.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage:"
+    echo "$0 dbname dbpass"
+    exit 1
+fi
+
 read -s -p "MySQL password: " PASS
 
 
 mysql -p$PASS <<EOF
-DROP DATABASE IF EXISTS dbvtiger;
-CREATE DATABASE dbvtiger DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL ON dbvtiger.* to 'dbvtiger'@'localhost' IDENTIFIED BY 'VtiG3rSDB1';
+DROP DATABASE IF EXISTS $1;
+CREATE DATABASE $1 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+GRANT ALL ON dbvtiger.* to '$1'@'localhost' IDENTIFIED BY '$2';
 EOF
 
-if [ -z $1 ]; then
+echo "..."
+echo "INFO: Successfully created DB $1 with user $1 and given pw"
+
+
+if [ -z $3 ]; then
     echo "INFO: Skipped injecting migration source."
-    echo "Give a path as first argument. Like:"
-    echo " $0 /here/is/my/gigration_source.sql"
+    echo "      Give a path as first argument. Like:"
+    echo "      $0 dbname password migration_src.sql"
     exit 0
 fi
 
-mysql -p$PASS dbvtiger < $1
+mysql -p$PASS $1 < $3
 
 
 
