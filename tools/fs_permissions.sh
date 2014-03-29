036e358843fb1b69c8285001d15a183e781eff96
Steffen Kockel <an@steffen-kockel.de>
2014-03-29 18:58:41 +0100

    changed helperscript

--- tools/fs_permissions.sh
+++ tools/fs_permissions.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+############################################
+#					   #
+#  D A N G E R !   			   #
+#					   #
+# this makes the whole directory writeable #
+# by the webserver. Make shure to correct  #
+# at least the parent folders permissions  #
+# or run fs_lock.sh 			   #
+#					   #
+############################################
+
 if [ -z $1 ]; then 
-    echo "Usage: "$1" /path/to/vtiger"
+    echo "Usage: "$1" /path/to/vtiger [webserver-runuser]"
     exit 1
 fi
+
+WWWUSER="$2"
+if [ -z $2 ]; then
+    WWWUSER="www-data"
+fi
+   
 
 cd $1
 
 mkdir -p cache/import/ backup modules/Webmails/tmp/
-chown root:www-data cache/import/ backup modules/Webmails/tmp
 
 echo "" > config.inc.php # just in case ...
 
-parentdir="$(dirname "$1")"
-
-# this makes the whole directory writeable
-# by the webserver. Make shure to correct 
-# at least the parent folders permissions 
-# or run fs_lock.sh 
-chown -R :www-data ../$parentdir
-chmod -R g+w,g+x ../$parentdir
+chown -R $WWWUSER:$WWWUSER $1
+chmod -R 777 $1
 
